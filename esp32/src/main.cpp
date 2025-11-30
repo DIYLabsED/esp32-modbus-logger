e306075ba1ca928f556e879fe9e0fb52d3852821
DIYLabsED <DIYLabsED@users.noreply.github.com>
2025-11-30 11:38:28 +0530

    Connect to WiFi network and print IP

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,13 +1,31 @@
 #include <Arduino.h>
+#include <ESPAsyncWebServer.h>
+#include <WiFi.h>
+
+#include <credentials.hpp>
+
+AsyncWebServer asyncServer(80);
+
 
 void setup(){
 
+  Serial.begin(115200);
 
+  Serial.println("Attempting to connect to " + String(WIFI_SSID) + " with password " + String(WIFI_PASSWORD));
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while(WiFi.status() != WL_CONNECTED){
+
+    Serial.println("Awaiting connection");
+    delay(500);
+
+  }
+
+  Serial.print("Connected. IP address: ");
+  Serial.println(WiFi.localIP());
 
 }
 
 void loop(){
 
-
-  
 }
