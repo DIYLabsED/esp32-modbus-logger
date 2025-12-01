36fd28f251f928f524aa809b42fc487850750afd
DIYLabsED <DIYLabsED@users.noreply.github.com>
2025-12-01 11:06:28 +0530

    Implement multi-user authenticated API as library

---
+++ esp32/lib/AuthAPI/AuthAPI.cpp
@@ -0,0 +1,63 @@
+#include <AuthAPI.hpp>
+
+AuthAPI::AuthAPI(AsyncWebServer *server){
+
+  _server = server;
+
+}
+
+AuthAPIPermissionLevel AuthAPI::_getClientLevel(AsyncWebServerRequest *request){
+
+  for(AuthAPIUser user : _users){
+
+    if(request->authenticate(user.username, user.password)){
+      return user.permissionLevel;
+    }
+
+  }
+
+  return -1;
+
+}
+
+void AuthAPI::registerUser(AuthAPIUser user){
+
+  _users.push_back(user);
+
+}
+
+void AuthAPI::registerEndpoint(AuthAPIEndpoint endpoint){
+
+  _server->on(endpoint.url, HTTP_GET,
+
+    [this, endpoint](AsyncWebServerRequest *request){
+
+      AuthAPIPermissionLevel clientLevel = _getClientLevel(request);
+
+      if(clientLevel >= endpoint.permissionLevel){
+        
+        endpoint.handler(request);
+
+      }
+      else{
+
+        if(clientLevel == -1){
+
+          request->requestAuthentication();
+          return;
+
+        }
+        else{
+
+          request->send(403, "text/plain", "Insufficient permission to access this endpoint.");
+          return;
+          
+        }
+
+      }
+
+    }
+
+  );
+
+}
---
+++ esp32/lib/AuthAPI/AuthAPI.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <ESPAsyncWebServer.h>
+#include <vector>
+
+typedef uint8_t AuthAPIPermissionLevel;
+
+struct AuthAPIUser{
+
+  const char *username;
+  const char *password;
+  AuthAPIPermissionLevel permissionLevel;
+
+};
+
+struct AuthAPIEndpoint{
+
+  const char *url;
+  ArRequestHandlerFunction handler;
+  AuthAPIPermissionLevel permissionLevel; 
+
+};
+
+class AuthAPI{
+
+public:
+  AuthAPI(AsyncWebServer *server);
+  void registerUser(AuthAPIUser user);
+  void registerEndpoint(AuthAPIEndpoint endpoint);
+
+private:
+  AsyncWebServer *_server;
+  std::vector<AuthAPIUser> _users;
+  AuthAPIPermissionLevel _getClientLevel(AsyncWebServerRequest *request);
+
+};
